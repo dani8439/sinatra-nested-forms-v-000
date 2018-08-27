1ed624a14ea40768ccc0ca4a8bce5919729856f0
Dani Schuhman <dani8439@aol.com>
2018-08-27 16:16:20 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,8 +1,9 @@
 class Ship
   attr_accessor :name, :type, :booty
+  SHIPS = []
 
   def initialize(name, type, booty)
-    @name = name 
-    @type = type 
+    @name = name
+    @type = type
     @booty = booty
 end
