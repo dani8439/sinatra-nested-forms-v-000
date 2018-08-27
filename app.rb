cecb714a2410433b2e71906f501c002248b63efb
Dani Schuhman <dani8439@aol.com>
2018-08-27 16:58:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,18 +13,6 @@ module FormsLab
       erb :'pirates/new'
     end
 
-    post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-
-      @ships = Ship.all
-
-      erb :'pirates/show'
-    end
-
 
   end
 end
